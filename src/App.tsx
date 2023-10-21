4a446751ef606321eba3009109592a708a39b876
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-22 00:23:25 +0400

    Adding routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { Header } from "./components/Header";
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom"; // Use Routes and Route from react-router-dom
+import Header from "./components/Header";
+import Recommended from "./components/Recommended";
+import SavedMovies from "./components/Bookmarks"; // Import the SavedMovies component
+import "./index.css"
+
+// ... (Movie and App component declarations remain the same)
 
 function App() {
+  const [savedMovies, setSavedMovies] = useState<Movie[]>([]);
+
+  const handleBookmarkClick = (movie: Movie) => {
+    setSavedMovies((prevSavedMovies) => [...prevSavedMovies, movie]);
+  };
 
   return (
     <div>
       <Header />
-      
+      <Routes>
+        <Route path="/" element={<Recommended onBookmarkClick={handleBookmarkClick} />} />
+        <Route path="/bookmarks" element={<SavedMovies savedMovies={savedMovies} />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
