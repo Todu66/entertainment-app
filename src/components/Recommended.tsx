43d28ffdd773cbf0f9849c0185406aeb8eaae122
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-28 21:24:34 +0400

    adding loading screen, default movies and more...

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,43 +1,52 @@
-import React, { useState } from "react";
-import data from "../../data";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-
-interface Movie {
-  name: string;
-  year: string;
-  category: string;
-  category2: string;
-  img: string;
-}
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { Movie } from "./Header"; // Import the Movie interface
 
 interface RecommendedProps {
   onBookmarkClick: (movie: Movie) => void;
+  initialData: Movie[]; // Pass initial data as a prop
 }
 
-const Recommended = (props: RecommendedProps) => {
-  const { onBookmarkClick } = props;
-  const [savedMovies, setSavedMovies] = useState<Movie[]>([]);
+const Recommended: React.FC<RecommendedProps> = (props) => {
+  const { onBookmarkClick, initialData } = props;
+  const [data, setData] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const handleBookmarkClick = (movie: Movie) => {
-    setSavedMovies((prevSavedMovies) => [...prevSavedMovies, movie]);
-    onBookmarkClick(movie);
-  };
+  useEffect(() => {
+    // Simulate an API call to fetch data
+    setTimeout(() => {
+      setData(initialData);
+      setLoading(false);
+    }, 1000); // Assuming it takes 1 second to fetch data
+  }, [initialData]);
+
+  if (loading) {
+    return <div className="flex justify-center items-center h-[100vh] text-white text-[50px]">
+      <FontAwesomeIcon icon={faSpinner} spinPulse />
+    </div>;
+  }
 
   return (
     <div className="text-[#fff] p-3">
+      {/* Mapping through the data received via props */}
       {data.map((card, index) => (
-        <div key={index}>
-          <img src={card.img} alt={card.name} />
-          <h2>{card.name}</h2>
-          <p>Year: {card.year}</p>
-          <p>Category: {card.category}</p>
-          <p>Rating: {card.category2}</p>
-          <FontAwesomeIcon
-            className="cursor-pointer"
-            onClick={() => handleBookmarkClick(card)}
-            icon={faBookmark}
-          />
+        <div className="w-44 mb-3" key={index}>
+          <div className="w-max">
+            <img src={card.img} alt={card.name} />
+            <h2>{card.name}</h2>
+            <p>Year: {card.year}</p>
+            <p>Category: {card.category}</p>
+            <div className="flex items-center justify-between">
+              <p>Rating: {card.category2}</p>
+              <FontAwesomeIcon
+                className="cursor-pointer"
+                onClick={() => onBookmarkClick(card)}
+                icon={faBookmark}
+              />
+            </div>
+          </div>
         </div>
       ))}
     </div>
