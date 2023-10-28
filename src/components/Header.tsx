89a2f155826a888070da4bdd62d2425ec9e6e075
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-28 21:25:17 +0400

    filter method to input and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import movie from "../assets/Movie.png";
 import group from "../assets/otx.png";
@@ -6,35 +6,63 @@ import rec from "../assets/doted.png";
 import tv from "../assets/tv.png";
 import saved from "../assets/bookmark.png";
 import profile from "../assets/person-header-image.png";
-import Bookmarks from "../components/Bookmarks"
-const Header = () => {
+import Bookmarks from "../components/Bookmarks";
+import { Movie } from "./Recommended";
+import data from "../../data";
+
+
+interface HeaderProps {
+  
+  onFilter: (filteredData: Movie[]) => void;
+}
+
+
+const Header = (props: HeaderProps) => {
+  const { onFilter } = props;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filterBySearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const query = event.target.value;
+    console.log("Query:", query)
+    let updatedList = [...data];
+    updatedList = updatedList.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase());
+    });
+
+    onFilter(updatedList); // Pass filtered data back to parent component
+    setSearchQuery(query); // Optionally, update local state if needed
+  };
+
+
   return (
     <div className="">
+      {/* -------------start of nav------------- */}
       <nav className="flex flex-row justify-between p-2 bg-[#161D2F]">
         <img src={movie} alt="movie" />
         <div className="flex gap-3 bg-[#161D2F]">
-          <Link to="/">
-          <img className="w-6 h-full" src={group} alt="group" />
-
+          <Link to="/recomended">
+            <img className="w-6 h-full" src={group} alt="group" />
           </Link>
           <img className="w-6" src={rec} alt="group" />
           <img className="w-6" src={tv} alt="group" />
-          <div
-            className="relative w-6 cursor-pointer"
-            
-          >
-             {/* <Link to="/bookmarks">Bookmarks</Link> */}
-             <Link to="/bookmarks">
-            <img className="w-full" src={saved} alt="bookmark" />
-
-             </Link>
+          <div className="relative w-6 cursor-pointer">
+            {/* <Link to="/bookmarks">Bookmarks</Link> */}
+            <Link to="/bookmarks">
+              <img className="w-full" src={saved} alt="bookmark" />
+            </Link>
           </div>
         </div>
         <img className="w-6 rounded-2xl" src={profile} alt="profile" />
       </nav>
+      {/* // ---------------end of nav--------------- */}
+
       <input
         className="w-4/5 text-[#fff] outline-none"
         placeholder="Search for movies or TV series"
+        
+        value={searchQuery}
+        onChange={filterBySearch}
       />
     </div>
   );
