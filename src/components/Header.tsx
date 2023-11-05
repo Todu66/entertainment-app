c0697d3fe4f3294d5b82430183a3bec8d5ef5750
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-05 23:40:02 +0400

    adding the link for transition to other components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,25 +6,22 @@ import rec from "../assets/doted.png";
 import tv from "../assets/tv.png";
 import saved from "../assets/bookmark.png";
 import profile from "../assets/person-header-image.png";
-import Bookmarks from "../components/Bookmarks";
+
 import { Movie } from "./Recommended";
+
 import data from "../../data";
 
-
 interface HeaderProps {
-  
   onFilter: (filteredData: Movie[]) => void;
 }
-
 
 const Header = (props: HeaderProps) => {
   const { onFilter } = props;
   const [searchQuery, setSearchQuery] = useState("");
 
   const filterBySearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    
     const query = event.target.value;
-    console.log("Query:", query)
+
     let updatedList = [...data];
     updatedList = updatedList.filter((item) => {
       return item.name.toLowerCase().includes(query.toLowerCase());
@@ -34,18 +31,22 @@ const Header = (props: HeaderProps) => {
     setSearchQuery(query); // Optionally, update local state if needed
   };
 
-
   return (
     <div className="">
       {/* -------------start of nav------------- */}
       <nav className="flex flex-row justify-between p-2 bg-[#161D2F]">
         <img src={movie} alt="movie" />
         <div className="flex gap-3 bg-[#161D2F]">
-          <Link to="/recomended">
+          <Link to="/Recommended">
             <img className="w-6 h-full" src={group} alt="group" />
           </Link>
-          <img className="w-6" src={rec} alt="group" />
-          <img className="w-6" src={tv} alt="group" />
+          <Link to="/movies">
+            <img className="w-6" src={rec} alt="group" />
+          </Link>
+          <Link to="/TvSeries">
+            <img className="w-6" src={tv} alt="group" />
+          
+          </Link>
           <div className="relative w-6 cursor-pointer">
             {/* <Link to="/bookmarks">Bookmarks</Link> */}
             <Link to="/bookmarks">
@@ -60,7 +61,6 @@ const Header = (props: HeaderProps) => {
       <input
         className="w-4/5 text-[#fff] outline-none"
         placeholder="Search for movies or TV series"
-        
         value={searchQuery}
         onChange={filterBySearch}
       />
