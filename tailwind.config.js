b96229d8b2a7473b1da459aeb442be61938162a0
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-06 18:53:18 +0400

    adding custom screen width

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "sm": "400px",
+        "custom": "700px",
+
+      }
+    },
   },
   plugins: [],
 }
