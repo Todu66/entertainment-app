d7579ad5147c2cb8f811faaa188458607e024b8e
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-08 23:13:12 +0400

    specifying type of savedMovies prop

--- src/components/Bookmarks.tsx
+++ src/components/Bookmarks.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-interface SavedMoviesProps {
+interface Movie {
   name: string;
   year: string;
   category: string;
   category2: string;
   img: string;
+}
 
+interface SavedMoviesProps {
+  savedMovies: Movie[]; // Specify the type for savedMovies prop as an array of Movie objects
 }
 
 const SavedMovies: React.FC<SavedMoviesProps> = ({ savedMovies }) => {
   return (
-    <div className="text-[#fff] p-3 sm:grid grid-cols-2 place-items-center">
+    <div className="text-[#fff] p-3 sm:grid grid-cols-2 place-items-center lg:w-customwidth">
       {savedMovies.map((movie, index: number) => (
         <div className="mb-3" key={index}>
           <img src={movie.img} alt={movie.name} />
