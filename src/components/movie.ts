210527c747fb8387ec616116a19feb8d4f309be8
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-09 20:06:22 +0400

    types for Movie data

---
+++ src/components/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: number;
+    name: string;
+    img: string;
+    year: number | string;
+    category: string;
+    category2: string;
+    marked: boolean;
+    
+}
