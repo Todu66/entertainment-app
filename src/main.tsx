c21c20f74291b47db0063fb7ee3468f0fb655f27
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-18 15:51:56 +0400

    adding create root for react18

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App'; // Your main application component
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App"; // Your main application component
 
-
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <Router>
-    
-      <App />
-   
-  </Router>,
-  document.getElementById('root')
+    <App />
+  </Router>
 );
