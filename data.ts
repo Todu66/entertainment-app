6baabe5de800eacb0980f489a2b35b8d6e40da95
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-11-18 15:50:37 +0400

    fixing module export typescript

---
+++ data.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+  name: string;
+  year: string;
+  category: string;
+  category2: string;
+  img: string;
+  marked: boolean;
+}
+
+const data: Movie[] = []; // Initialize with your data
+export default data;
